4ee3fbc659cc221be7dff5d686771609af8a8717
hainguyen03042000 <134134989+hainguyen03042000@users.noreply.github.com>
2023-10-18 22:25:48 +0700

    Update config.js
    lần 1

--- config.js
+++ config.js
@@ -3,17 +3,17 @@ var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的小可爱",  // 同上...
-        "今天是你的生日",
-        "这是我们在一起的",
-        "第三个生日了哦",
-        "去年的生日",
-        "还记得在王婆串串吃的呢",
-        "今年要吃好的喽哦",
-        "要把我家可爱猪猪喂饱饱",
-        "然后抱走",
-        "YAMI~~",
+        "haha. Lại già thêm một tuổi rồi!",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "Chúc mừng sinh nhật bà nhé",  // 同上...
+        "Tuổi mới hay ăn chóng lớn",
+        "À tiện thể mai 20/10",
+        "Nên chúc luôn",
+        "Chúc bà 20/10 vui vẻ",
+        "Tình hình là tôi hết văn rồi",
+        "Mà cái mẫu này dài quá :)",
+        "Biết Nói gì nữa đây nhỉ",
+        "À Biết rồi",
+        "Linh lợn",
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -25,18 +25,22 @@ var config = {
      * Tip: 图片最好用正方形or接近正方形, 看起来效果更好
      */
     imgs: {
-        "心爱的小可爱": "./imgs/xiaokeai.png",
+        "Linh lợn": "./imgs/lon.jpg",
+        "Chúc mừng sinh nhật bà nhé": "./imgs/linh.png",
+        "Tuổi mới hay ăn chóng lớn": "./imgs/linh2.png",
+        "haha. Lại già thêm một tuổi rồi!": "./imgs/linh3.png",
+
         // "今天是你的生日": "./imgs/birthday.jpg",
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
-        story: "A MESSAGE FOR YOU",
+        turn_on: "Bật đèn nào!",
+        play: "Bật nhạc!",
+        bannar_coming: "Trang trí nè!",
+        balloons_flying: "thổi bóng bay",
+        cake_fadein: "Không thể thiếu bánh sinh nhật",
+        light_candle: "Thắp nến",
+        wish_message: "Chúc mừng sinh nhật",
+        story: "Lời chúc Ahihi",
     }
 };
